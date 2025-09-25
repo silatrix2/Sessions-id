8fd6ba7c6d6dc25851ae9e38533142f1ece798f3
silatrix2 <silatrix22@gmail.com>
2025-09-25 03:39:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-__path = process.cwd()
-const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8000;
-let server = require('./qr'),
-    code = require('./pair');
+
+const __path = process.cwd(); // current working directory
+const bodyParser = require('body-parser');
+
+// increase listeners early if you need it
 require('events').EventEmitter.defaultMaxListeners = 500;
-app.use('/server', server);
-app.use('/code', code);
-app.use('/pair',async (req, res, next) => {
-res.sendFile(__path + '/pair.html')
-})
-app.use('/qr',async (req, res, next) => {
-res.sendFile(__path + '/qr.html')
-})
-app.use('/',async (req, res, next) => {
-res.sendFile(__path + '/main.html')
-})
+
+// Load modules (assumes these export express routers or middleware)
+let server = require('./qr');
+let code = require('./pair');
+
+const PORT = process.env.PORT || 8000;
+
+// parse request bodies BEFORE routes
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// serve static assets if you have a public folder (optional)
+// place css, js, images in a 'public' folder
+app.use(express.static(path.join(__path, 'public')));
+
+// mount routers (if server and code are routers)
+app.use('/server', server);
+app.use('/code', code);
+
+// simple GET handlers to serve HTML files
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__path, 'pair.html'));
+});
+
+app.get('/qr', (req, res) => {
+  res.sendFile(path.join(__path, 'qr.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__path, 'main.html'));
+});
+
+// start server
 app.listen(PORT, () => {
-    console.log(`
-Don't Forgot To Give Star 𝚂𝙸𝙻𝙰𝚃𝚁𝙸𝚇-𝙼𝙳 
+  console.log(`
+Don't Forget To Give Star 𝚂𝙸𝙻𝙰𝚃𝚁𝙸𝚇-𝙼𝙳
 
- Server running on http://localhost:` + PORT)
-})
+Server running on http://localhost:${PORT}
+`);
+});
 
-module.exports = app
+module.exports = app;
